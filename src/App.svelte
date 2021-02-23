cd631cbfbb67bb5b3cc0867a431a81573bcf2823
MsAFunk <62901425+MsAFunk@users.noreply.github.com>
2021-02-22 22:29:58 -0800

    Responsive progress 2

--- src/App.svelte
+++ src/App.svelte
@@ -13,9 +13,6 @@
 </script>
 
 <style>
-
-
-
 	main {width:100%; height:100vh; margin:0; padding:0;}
 	main:after {
         content: " ";
@@ -30,16 +27,28 @@
 				background-color:#464445; border-top:3px #000 solid;border-bottom:3px #000 solid;
 			}
 
-			.screenshots {width:30%; position:absolute; bottom:50%; right:3%; transform: translateY(50%); }
-				.screenshots ul { width:100%; list-style:none; margin:0; padding:0; text-align:right; vertical-align: middle;}
-					.screenshots li { margin:30px;}
-						.screenshots img {width:90%; max-width:350px; border:5px #464445 solid; outline:2px #000 solid;transition: all 0.2s ease;}
-						.screenshots img:hover {width:100%; max-width:400px; cursor:pointer;}
-			
 			.jack {background:url(/images/main-menu.jpg) no-repeat center center fixed; background-size:cover;}			
 				.jack .talk-bubble:before {background: url(/images/JACK.png) center no-repeat;	background-size:contain;}
 				.jack .talktext {background-color: #7D2222;}
 					.jack .tri-right.btm-left-in:after{	border-color:#7D2222 transparent transparent #7D2222;}
+			
+			.trailer {background: url(/images/main-menu.jpg) no-repeat center center fixed #000; background-size:cover;}
+				.background-filter {width:100%; height:100%; position:absolute; backdrop-filter: blur(100px) brightness(0.8);}
+				.video-embed {
+					width:80vw; height:80vh; max-width:1280px; max-height:720px; 
+					position: absolute; left:50%; top:50%; transform: translate(-50%, -50%); 
+					background: #000; border:2px #000 solid; outline:10px #464445 solid; box-shadow: 0 0 0 12px rgba(0,0,0,1)
+				}
+
+	@media all and (max-width:1365px) {
+		.buster, .andrea, .lena {display:none;}
+	}
+	@media all and (min-width:1366px) {
+		.screenshots {width:30%; position:absolute; bottom:50%; right:3%; transform: translateY(50%); }
+				.screenshots ul { width:100%; list-style:none; margin:0; padding:0; text-align:right; vertical-align: middle;}
+					.screenshots li { margin:30px;}
+						.screenshots img {width:90%; max-width:350px; border:5px #464445 solid; outline:2px #000 solid;transition: all 0.2s ease;}
+						.screenshots img:hover {width:100%; max-width:400px; cursor:pointer;}
 			
 			.buster {background:url(/images/wastes-day.jpg) no-repeat center center fixed; background-size:cover; overflow:hidden;}			
 				.buster .talk-bubble:before {background: url(/images/BUSTER.png) center no-repeat;  background-size:contain;}
@@ -55,14 +64,8 @@
 				.lena .talk-bubble:before {background: url(/images/LENA.png) center no-repeat; background-size:contain;}
 				.lena .talktext {background-color: #369136;}
 					.lena .tri-right.btm-left-in:after	{border-color:#369136 transparent transparent #369136;}
-
-			.trailer {background: url(/images/main-menu.jpg) no-repeat center center fixed #000; background-size:cover;}
-				.background-filter {width:100%; height:100%; position:absolute; backdrop-filter: blur(100px) brightness(0.8);}
-				.video-embed {
-					width:80vw; height:80vh; max-width:1280px; max-height:720px; 
-					position: absolute; left:50%; top:50%; transform: translate(-50%, -50%); 
-					background: #000; border:2px #000 solid; outline:10px #464445 solid; box-shadow: 0 0 0 12px rgba(0,0,0,1)
-				}
+	}
+			
 
 </style>
 
@@ -81,9 +84,8 @@
 		</div>
 
 		<a class="CTA-btn" href="https://www.kickstarter.com/projects/razburygames/wastenauts" target="_blank">Now on Kickstarter!</a>
+		<div class="divider"></div>
 	</div>
-	
-	<div class="divider"></div>
 	
 	<div class="comic-panel trailer">
 		<div class="background-filter"></div>
@@ -91,10 +93,11 @@
 		<div class="video-embed">
 			<iframe title="Wastenauts Announcement Trailer" style="width:100%; height:100%;" src="https://www.youtube.com/embed/JEBM7ErIPJs" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-		</div>	
+		</div>
+		<div class="divider"></div>
 	</div>
 
-	<div class="divider"></div>
+	
 	
 	<div class="comic-panel buster">
 		<div class="talk-bubble">
@@ -110,9 +113,10 @@
 				<li on:click={() => toggleModal(3)}><img src="images/3-thumb.jpg" alt=""></li>
 			</ul>
 		</div>
+		<div class="divider"></div>
 	</div>
 
-	<div class="divider"></div>
+	
 	
 	<div class="comic-panel andrea">
 		<div class="talk-bubble">
@@ -129,10 +133,10 @@
 				<li on:click={() => toggleModal(6)}><img src="images/6-thumb.jpg" alt=""></li>
 			</ul>
 		</div>
-
+		<div class="divider"></div>
 	</div>
 
-	<div class="divider"></div>
+	
 	
 	<div class="comic-panel lena">
 		<div class="talk-bubble">
@@ -149,7 +153,7 @@
 				<li on:click={() => toggleModal(9)}><img src="images/9-thumb.jpg" alt=""></li>
 			</ul>
 		</div>
-
+		<div class="divider"></div>
 	</div>
 
 	
